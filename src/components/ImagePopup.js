440d580a4d5505caf8dc6df08a8b7e1680eb851d
Vova Gridnev <gridnevmami@gmail.com>
2021-09-05 22:33:58 +0300

    fix: третье ревью 11 проектная работа

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ function ImagePopup({ card, onClose }) {
     return(
         <div className={`popup ${card.link ? 'popup_open' : ''}`} onClick={onClose}>
             <div className="popup__image-container">
-                <button className="popup__button-close" type="button"></button>
+                <button className="popup__button-close" type="button" onClick={onClose}></button>
                 <img className="popup__image" src={card.link} alt={card.name}/>
                 <p className="popup__image-text">{card.name}</p>
             </div>
